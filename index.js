a8588e27d86b660860829b56ba8956c924d8dd87
lisapeters13 <lisapetersirl@gmail.com>
2024-07-07 14:20:40 -0400

    Initial commit

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+function submitData(name, email) {
+    const configurationObject = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({
+        name: name,
+        email: email
+      })
+    };
+  
+    return fetch("http://localhost:3000/users", configurationObject)
+      .then(response => response.json())
+      .then(data => {
+        const newId = data.id;
+        const idElement = document.createElement("p");
+        idElement.textContent = newId;
+        document.body.appendChild(idElement);
+      })
+      .catch(error => {
+        const errorMessage = error.message;
+        const errorElement = document.createElement("p");
+        errorElement.textContent = errorMessage;
+        document.body.appendChild(errorElement);
+      });
+  }
